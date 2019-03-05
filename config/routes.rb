b1f2743df4d077ac2f54982107eedce9d4f62f4a
QuentinPrevost2 <prevostq@gmail.com>
2019-03-05 19:50:06 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   get '/welcome', to: 'welcomes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
 
   resources :golfs
 
-=======
   resources :carsharings
->>>>>>> carsharing_views
 end
 
