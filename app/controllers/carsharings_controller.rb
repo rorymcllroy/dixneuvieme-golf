05672e1a310b7e75007b3c3081fcefc22e296af8
QuentinPrevost2 <prevostq@gmail.com>
2019-03-05 19:28:15 +0100

    carsharing index view

--- app/controllers/carsharings_controller.rb
+++ app/controllers/carsharings_controller.rb
@@ -1,40 +1,39 @@
 class CarsharingsController < ApplicationController
-end
-
-def index
-  @carsharing = Carsharing.all
-end
-
-
-def show
-  @carsharing = Carsharing.find(params[:carsharing_id])
-end
-
-def new
-  @carsharing = Carsharing.new
-end
-
-def create
-  @carsharing = Carsharing.new(carsharing_id: params[:carsharing_id], driver_id: params[:driver_id], date: params[:date], available_places: params[:available_places], description: params[:description], price: params[:price], golf_id: params[:golf_id])
-
+  
+  def index
+    @carsharing = Carsharing.all
+  end
+  
+  def show
+    @carsharing = Carsharing.find(params[:carsharing_id])
+  end
+  
+  def new
+    @carsharing = Carsharing.new
+  end
+  
+  def create
+    @carsharing = Carsharing.new(carsharing_id: params[:carsharing_id], driver_id: params[:driver_id], date: params[:date], available_places: params[:available_places], description: params[:description], price: params[:price], golf_id: params[:golf_id])
+    
     if @carsharing.save # save carsharing in the db
       redirect_to carsharings_path, :notice => "Nouveau covoiturage créé !"
     else
       render 'new'
     end
-end
-
-
-def edit
-   @carsharing = Carsharing.find(params[:id])
-end
-
-def update
-##
-end
-
-def destroy
-  @carsharing = Carsharing.find(params[:id])
-  @carsharing.destroy
-  redirect_to carsharings_path
+  end
+  
+  
+  def edit
+    @carsharing = Carsharing.find(params[:id])
+  end
+  
+  def update
+    ##
+  end
+  
+  def destroy
+    @carsharing = Carsharing.find(params[:id])
+    @carsharing.destroy
+    redirect_to carsharings_path
+  end
 end
