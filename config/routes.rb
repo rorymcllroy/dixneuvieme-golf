5af6d3bdfdcbfb2213041fbf160a8ef2fa717bc3
Mcrespo42 <maxime.crespopro@gmail.com>
2019-03-05 12:22:14 +0100

    mail prod with sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'test#index'
-  get 'test/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
