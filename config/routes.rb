3cdeb2ee94a7736a8f10ed651d710dbae4e3f15a
QuentinPrevost2 <prevostq@gmail.com>
2019-03-06 12:56:13 +0100

    connected/not connected

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get '/welcome', to: 'welcomes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'welcomes#index' 
   resources :golfs
-
   resources :carsharings
 end
 
