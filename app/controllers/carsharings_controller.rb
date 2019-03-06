4dac8852115cae77e4642a96770de9238fc75493
QuentinPrevost2 <prevostq@gmail.com>
2019-03-06 11:48:44 +0100

    first carsharing creation form view

--- app/controllers/carsharings_controller.rb
+++ app/controllers/carsharings_controller.rb
@@ -13,13 +13,17 @@ class CarsharingsController < ApplicationController
   end
   
   def create
-    @carsharing = Carsharing.new(carsharing_id: params[:carsharing_id], driver_id: params[:driver_id], date: params[:date], available_places: params[:available_places], description: params[:description], price: params[:price], golf_id: params[:golf_id])
+    puts "<><><><><><><><><><><><><>"
+    puts params
+    puts "<><><><><><><><><><><><><>"
+    redirect_to carsharings_path
+    #@carsharing = Carsharing.new(driver_id: 1, date: params[:start_date], available_places: params[:available_places], description: params[:description], price: params[:price], golf_id: params[:golf_id])
     
-    if @carsharing.save # save carsharing in the db
-      redirect_to carsharings_path, :notice => "Nouveau covoiturage créé !"
-    else
-      render 'new'
-    end
+    #if @carsharing.save # save carsharing in the db
+    #  redirect_to carsharings_path, :notice => "Nouveau covoiturage créé !"
+    #else
+    #  render 'new'
+    #end
   end
   
   
