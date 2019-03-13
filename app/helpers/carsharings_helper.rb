777fef9901dfb6f305a54d3d478f1a19ebdda5a1
QuentinPrevost2 <prevostq@gmail.com>
2019-03-13 17:34:50 +0100

    user profile with detailed carsharing

--- app/helpers/carsharings_helper.rb
+++ app/helpers/carsharings_helper.rb
@@ -1,2 +1,5 @@
 module CarsharingsHelper
+  def carsharing_as_driver(user_id)
+    Carsharing.all.where(driver_id: user_id).count
+  end
 end
--- app/helpers/passengers_helper.rb
+++ app/helpers/passengers_helper.rb
@@ -2,4 +2,7 @@ module PassengersHelper
   def passengers_booked(carsharing_id)
     Passenger.all.where(carsharing_id: carsharing_id).count
   end
+  def carsharing_as_passenger(user_id)
+    Passenger.all.where(passenger_id: user_id).count
+  end
 end
