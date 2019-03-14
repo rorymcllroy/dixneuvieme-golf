7740b3b54dd1465f8ef732a6d6dcdd3789d66324
Mcrespo42 <maxime.crespopro@gmail.com>
2019-03-14 11:22:45 +0100

    AJAX WORK !!!

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,8 +4,11 @@ class ConversationsController < ApplicationController
   def create
     @new_pm = Privatemessage.new(content: params[:content], recipient_id: params[:recipient], sender_id: params[:user_id])
     if @new_pm.save
-      flash[:success] = "Message envoyé avec succès"
-      redirect_to user_conversation_path(current_user, params[:recipient])
+      respond_to do |format|
+        format.html { redirect_to user_conversation_path(current_user, params[:recipient]) }
+        format.js
+        flash[:success] = "Message envoyé avec succès"
+      end
     else
       flash[:danger] = "Erreur dans l'envoi du message"
       redirect_to new_user_private_message_path(current_user)
