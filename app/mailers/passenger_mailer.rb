93680ac01bb25740e6e3c39e19f512ca72e4f5d6
vincelep <vincent.leparoux@gmail.com>
2019-03-14 16:30:18 +0100

    modification views passenger_mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,11 +2,11 @@ class PassengerMailer < ApplicationMailer
 
     default from: 'no-reply@dixneuviemetrou.fr'
 
-    def confirmed_reservation_email(user)
-      @user = user
-      @url  = 'http://localhost:3000/'
-      mail(to: @user.email, subject: 'Réservation covoiturage !')
+    def new_passenger_email(passenger)
+        @carsharing = passenger.carsharing
+        @passenger = passenger.passenger
+        @url  = 'http://localhost:3000/'
+      mail(to: @passenger.email, subject: 'Réservation covoiturage !')
     end
 
 end
-
