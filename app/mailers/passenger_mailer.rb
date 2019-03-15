9ffe77571f9cf488e0fd7c9db9e154631cd72571
vincelep <vincent.leparoux@gmail.com>
2019-03-15 13:02:58 +0100

    config carsharing-preview-email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,19 +1,26 @@
 class PassengerMailer < ApplicationMailer
-
-    default from: 'no-reply@dixneuviemetrou.fr'
-
-    def new_passenger_email(passenger)
-        @carsharing = passenger.carsharing
-        @passenger = passenger.passenger
-        @url  = 'http://localhost:3000/'
-      mail(to: @passenger.email, subject: 'Réservation covoiturage !')
-    end
-
-    def new_reservation_passenger_email(passenger)
-      @carsharing = passenger.carsharing
-      @passenger = passenger.passenger
-      @url  = 'http://localhost:3000/'
+  
+  default from: 'no-reply@dixneuviemetrou.fr'
+  
+  def new_passenger_email(passenger)
+    @carsharing = passenger.carsharing
+    @passenger = passenger.passenger
+    @url  = 'http://localhost:3000/'
+    mail(to: @passenger.email, subject: 'Réservation covoiturage !')
+  end
+  
+  def new_reservation_passenger_email(passenger)
+    @carsharing = passenger.carsharing
+    @passenger = passenger.passenger
+    @url  = 'http://localhost:3000/'
     mail(to: @carsharing.driver.email, subject: 'Réservation covoiturage !')
   end
-
+  
+  def carsharing_review_email(passenger)
+    @carsharing = passenger.carsharing
+    @passenger = passenger.passenger
+    @url  = 'http://localhost:3000/'
+    mail(to: @passenger.email, subject: 'Noter le covoiturage !')
+  end
+  
 end
