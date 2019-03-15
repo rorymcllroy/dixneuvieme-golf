b1f13187a6365724e7ae347bff79f09450b6b016
Mcrespo42 <maxime.crespopro@gmail.com>
2019-03-15 12:12:54 +0100

    create review worked

--- app/controllers/carsharing_reviews_controller.rb
+++ app/controllers/carsharing_reviews_controller.rb
@@ -3,11 +3,14 @@ class CarsharingReviewsController < ApplicationController
   end
 
   def create
-    @csr = CarsharingReview.new(content: params[:content], grade: params[:grade], carsharing_id: params[:carsharing_id], passenger_id: current_user)
+    @csr = Carsharingreview.new(content: params[:content], grade: params[:grade], carsharing_id: params[:carsharing_id], passenger_id: current_user.id)
     if @csr.save
       redirect_to root_path
     else
       redirect_to new_carsharing_carsharing_review_path(params[:carsharing_id])
     end
+  end
+
+  def index
   end
 end
