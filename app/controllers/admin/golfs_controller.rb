0070252d77fe5d48f9d8b4a20e0730a72d28c9cb
NIcoZF <nico-7@hotmail.fr>
2019-03-17 17:12:48 +0100

    admin golf edit form functional

--- app/controllers/admin/golfs_controller.rb
+++ app/controllers/admin/golfs_controller.rb
@@ -18,14 +18,18 @@ class Admin::GolfsController < AdminController
     end
   
     def update
-        if params[:validation] == "true"
-          puts "ok"
-          Golf.find(params[:id]).update(validated: true)
-        else
-          puts "nok"
-          Golf.find(params[:id]).update(validated: false)
-        end
-        redirect_to admin_carsharings_path
+      @golf = Golf.find(params[:id])
+
+      golf_params = params.permit(:name, :lat, :long, :description, :price_range, :course_type, :par, :length, :website)
+    
+
+      if @golf.update(golf_params)
+        flash[:success] = 'Le golf a été mis à jour!'
+        redirect_to admin_golfs_path
+      else
+        
+        redirect_to admin_golfs_path, alert: "Oops! There was a problem, please try again"
+      end
     end
      
     
@@ -35,5 +39,7 @@ class Admin::GolfsController < AdminController
       @golf.destroy
       redirect_to admin_golf_path
     end
+
+
   
  end
