4a124b50c67a9e73e40c960aeef86038e6467797
vincelep <vincent.leparoux@gmail.com>
2019-03-19 18:26:55 +0100

    modifications test carsharing_controller

--- spec/controllers/carsharings_controller_spec.rb
+++ spec/controllers/carsharings_controller_spec.rb
@@ -20,59 +20,12 @@ RSpec.describe CarsharingsController, type: :controller do
         expect(response).to have_http_status(:ok)
     end    
   end
-  
-  describe "GET show" do
-    it 'assigns @carsharings' do
-        carsharing = Carsharing.create
-        get :show, id: carsharing.id  
-        expect(assigns(:carsharing)).to eq(carsharing)
-    end
-  
-    it 'renders the show template' do
-        get :show
-        expect(response).to render_template("show")
-    end
-
-  end
 
   describe "GET new" do
-    it "renders the new template" do
-      get :new
-      expect(response).to render_template("new")
-    end
+  it "renders the new template" do
+    get :new
+    expect(subject).to render_template(:new)
   end
-
-  describe "GET edit" do
-    it "assigns @carsharings" do
-      # création d'une instance
-      carsharing = Carsharing.create
-  
-      # on va sur edit
-      get :edit, id: carsharing.id
-  
-      # @user doit être user
-      expect(assigns(:carsharing)).to eq(carsharing)
-    end
-  
-    it "renders the edit template" do
-      # va sur edit
-      get :edit
-  
-      # on doit rediriger vers edit
-      expect(response).to render_template("edit")
-    end
   end
-
-#   describe "POST #create" do
-#     # des tests
-#   end
-
-#   describe "PUT #update" do
-#     # des tests
-#   end
-
-#   describe "DELETE #destroy" do
-#     # des tests
-#   end
 
 end
