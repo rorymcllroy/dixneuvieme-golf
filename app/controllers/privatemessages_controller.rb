bc6ffffb5987703dce58c3f5ca83cff9890d061f
QuentinPrevost2 <prevostq@gmail.com>
2019-03-19 21:56:48 +0100

    edit user

--- app/controllers/privatemessages_controller.rb
+++ app/controllers/privatemessages_controller.rb
@@ -1,14 +1,10 @@
 class PrivatemessagesController < ApplicationController
   before_action :authenticate_user!
-  def index
-  end
-  def new
-  end
   def create
     @new_pm = Privatemessage.new(content: params[:content], recipient_id: params[:recipient][:id], sender_id: current_user.id)
     if @new_pm.save!
       flash[:success] = "Message envoyé avec succès"
-      redirect_to user_path(current_user)
+      redirect_to user_conversation_path(current_user, params[:recipient][:id])
     else
       flash[:danger] = "Erreur dans l'envoi du message"
       redirect_to new_user_private_message_path(current_user)
