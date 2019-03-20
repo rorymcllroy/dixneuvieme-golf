72e34621a039f59f942f73fe4adb3c32cc46625d
vincelep <vincent.leparoux@gmail.com>
2019-03-20 17:17:36 +0100

    modifications spec/controllers/carsharingreviews_controller_spec_test

--- spec/controllers/carsharingreviews_controller_spec.rb
+++ spec/controllers/carsharingreviews_controller_spec.rb
@@ -2,14 +2,8 @@ require 'rails_helper'
 
 RSpec.describe CarsharingreviewsController, type: :controller do
 
-  describe "GET new" do
+  describe "GET #new" do
     it "renders the new template" do
-      # va sur new
-      get :new
-  
-      # on doit rediriger vers new
-      expect(response).to render_template("new")
     end
   end
-
 end
