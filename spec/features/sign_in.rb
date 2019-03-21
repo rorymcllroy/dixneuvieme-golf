f6eca46a59e2d92b16dc5f46b728b703e8a91288
Mcrespo42 <maxime.crespopro@gmail.com>
2019-03-21 10:45:46 +0100

    features test visit not work ?

--- spec/features/sign_in.rb
+++ spec/features/sign_in.rb
@@ -1,3 +1,10 @@
 describe "the signin process", type: :feature do
-  
+  it 'sign me in' do
+    visit 'devise/sessions#new'
+    fill_in "user[email]", with: "testing@email.com"
+    fill_in "user[password]", with: "motdepasse"
+    fill_in "user[password_confirmation]", with: "motdepasse"
+    click_button "S'inscrire"
+    expect(page).to have_current_path(root_path)
+  end
 end
