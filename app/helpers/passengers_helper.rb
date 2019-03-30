091fd3e7be6055829e476fff58603cecd16a6fcf
QuentinPrevost2 <prevostq@gmail.com>
2019-03-30 15:00:56 +0100

    passenger card updated

--- app/helpers/passengers_helper.rb
+++ app/helpers/passengers_helper.rb
@@ -2,6 +2,9 @@ module PassengersHelper
   def passengers_booked(carsharing_id)
     Passenger.all.where(carsharing_id: carsharing_id).count
   end
+  def passenger_of_carsharing_array(carsharing_id)
+    Passenger.all.where(carsharing_id: carsharing_id)
+  end
   def carsharing_as_passenger(user_id)
     Passenger.all.where(passenger_id: user_id).count
   end
